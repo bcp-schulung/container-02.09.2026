02dbb315240b8b9e1861e4770478ed0967b8cbf7
Ben Cöppicus <ben.coeppicus@it-scholar.com>
2026-08-31 10:29:38 +0200

    Add initial exercise script for Docker commands

---
+++ exercises/exercise-1/exercise.sh
@@ -0,0 +1,19 @@
+sudo docker run -d -p 8282:80 nginx
+
+sudo docker ps
+
+curl localhost:8282
+
+sudo docker logs 00adcc35c5c4
+
+sudo docker run -d -p 8282:80 nginx
+
+sudo docker run -d -p 8383:80 nginx
+
+sudo docker ps -a
+
+sudo docker rm 8fe967db4f13
+
+sudo docker ps -a
+
+sudo docker images

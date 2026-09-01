929e3907a9d2d533c05b3841a75181b8455df3cc
Ben Cöppicus <ben.coeppicus@it-scholar.com>
2026-09-01 10:37:18 +0200

    Update Dockerfile and compose configuration for app service; set dynamic port and add healthcheck

--- exercises/exercise-5/exercise.sh
+++ exercises/exercise-5/exercise.sh
@@ -3,3 +3,9 @@ sudo docker build -t harbor.container.it-scholar.com/seminar/ben-coeppicus:v4 .
 sudo docker compose up -d
 
 curl http://localhost:9191
+
+sudo docker build -t harbor.container.it-scholar.com/seminar/ben-coeppicus:v5 --build-arg PORT=8484 .
+
+sudo docker compose up -d
+
+curl http://localhost:9191
